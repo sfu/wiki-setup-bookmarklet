6280ea74c117f58173d7975711eb1aee798c842e
Graham Ballantyne <grahamb@sfu.ca>
2012-01-03 14:37:19 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# SFU MediaWiki Setup Bookmarklet
+
+This bookmarklet simplifies setup of MediaWiki in SFU's environment. It autofills constant paramaters and prompts for a couple of variables (sitename and DB prefix) with suggested defaults.
+
+## Installation
+1. SysOp and DB passwords are not included in the source. Edit src.js and replace instances of "REPLACE\_ME" with the appropriate passwords.
+2. Change the default database if necessary.
+3. Run the build script (./build).
+4. The minified bookmarklet will be created as bookmarklet.js. Add it to your browser of choice.
+
+## Usage
+Invoke the bookmarklet from a MediaWiki config page. You will be prompted for the Sitename, DB name and DB prefix. Enter values (or accept the defaults). Check the form and if OK, click "Install MediaWiki" and proceed with the rest of the setup.
+
+## Notes
+The build script uses jsmin.c from Douglas Crockford (http://www.crockford.com/javascript/jsmin.c) to minify src.js. A compiled copy (using gcc on OS X 10.7.2) is included.
---
+++ src.js
@@ -0,0 +1,37 @@
+(function() {
+    
+    if (window.location.host !== 'wiki.sfu.ca' || window.location.pathname.indexOf('/config/') === -1) {
+        // not the config page, bail
+        alert('This script can only run from a wiki.sfu.ca config page.');
+        return false;
+    }
+    alert('hi');
+    var path = location.pathname.replace('/config/', '').substr(1).replace('/', '_');
+
+    var promptForValues = {
+        Sitename: path,
+        DBname: 'sfumediawikidb112x',
+        DBprefix: path + '_'
+    };
+
+    var fields = {
+        EmergencyContact: 'wiki@sfu.ca',
+        SysopPass: 'REPLACE_ME',
+        SysopPass2: 'REPLACE_ME',
+        DBserver: 'mara.nfs.sfu.ca',
+        DBuser: 'sfumediawiki',
+        DBpassword: 'REPLACE_ME',
+        DBpassword2: 'REPLACE_ME',
+        RootUser: ''
+    };
+
+    for (var promptField in promptForValues) {
+        fields[promptField] = prompt('Value for ' + promptField, promptForValues[promptField]);
+    }
+
+    for (var field in fields) {
+        document.getElementById(field).value = fields[field];
+    }
+    
+    window.scrollTo(0,document.body.scrollHeight);
+})();
